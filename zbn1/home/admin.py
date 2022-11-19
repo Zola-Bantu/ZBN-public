d9b4cdb11534427ce963efb471ce2aa4fca26433
nal01 <kagisokobue28@gmail.com>
2022-11-19 15:27:47 +0000

    Cleared profile table - now moving on to profile table api

--- zbn1/home/admin.py
+++ zbn1/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Mosebedisi
+from .models import Mosebedisi, Profile
 # Register your models here.
 
 
 admin.site.register(Mosebedisi)
+admin.site.register(Profile)
--- zbn1/home/models.py
+++ zbn1/home/models.py
@@ -1,13 +1,22 @@
 from django.db import models;
 
-# Create your models here.
+RELATIONSHIP_STATUS_CHOICES = (
+    ('SI', 'single'),
+    ('IR', 'in committed relationship'),
+    ('MA', 'married'),
+    ('SP', 'separated'),
+    ('DV', 'divorced'),
+    ('WI', 'widowed')
+    );
+    
+# Users table
 
 class Mosebedisi(models.Model):
-    username = models.CharField(max_length=50		# Maximum number of characters allowed for username.
-    				,blank=True, null=True # Allows username to be left blank.
+    username = models.CharField(max_length=50	# Maximum number of characters allowed for username.
+    				,blank=True, null=True      # Allows username to be left blank.
     				);
-    pkn = models.BigIntegerField(default=0);		# Public key n.
-    pke = models.BigIntegerField(default=0);		# Public key e.
+    pkn = models.CharField(max_length=150, default='0');	    # Public key n.
+    pke = models.CharField(max_length=50, default='0');	    # Public key e.
 	
     def __str__(self):
         return "%s" % (self.username);
@@ -16,14 +25,21 @@ class Mosebedisi(models.Model):
 #Profile table
 
 class Profile(models.Model):
-    Mosebedisi = models.OneToOneField(
+    mosebedisi = models.OneToOneField(
         Mosebedisi,
         on_delete=models.CASCADE,
-    primary_key=True,
+        primary_key=True,
         );
-    Profile Picture = models.FileField();
-    Female = models.BooleanField(default=True);
-    Status = models.CharField(max_length=50 ,blank=True, null=True);
-    Bio =  models.CharField(blank=True, null=True);
+    picture = models.FileField();
+    female = models.BooleanField(default=True);
+    Status = models.CharField(
+        max_length=2,
+        choices=RELATIONSHIP_STATUS_CHOICES, 
+        default='SI'
+        );
+    bio =  models.CharField(max_length=300, blank=True, null=True);
+	
+    def __str__(self):
+        return "%s" % (self.female);
 
 
