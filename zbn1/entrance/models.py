f9cd53197928de6f09cb411fbcd7adabb07bc209
Jessenwia23 <Jessenwia23@gmail.com>
2023-04-29 16:32:57 +0100

    Qrcode table created 29/04/2023

--- zbn1/entrance/models.py
+++ zbn1/entrance/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Qrcode(models.Model):
+    name = models.CharField(max_length=50	# Maximum number of characters allowed for username.
+    				,blank=True, null=True      # Allows username to be left blank.
+    				);
+    path = models.CharField(max_length=150);	    # Public key n.
+    timestamp = models.DateTimeField(auto_now_add=True);	    # Public key e.
+	
+    def __str__(self):
+        return "%s | created at %s" % (self.name, self.timestamp);
