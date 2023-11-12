aef3c3dac3ad484441cf2064f4b61f7bab1ef5a3
nal01 <kagisokobue28@gmail.com>
2023-11-12 15:23:51 +0200

    Added email signal and authtoken

--- zbn1/muntu/models.py
+++ zbn1/muntu/models.py
@@ -1,5 +1,5 @@
-from django.db import models;
 import datetime;
+from django.db import models;
 from django.db.models.signals import post_save, pre_save;
 from django.dispatch import receiver;
 from django.core.mail import send_mail;
